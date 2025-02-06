3f6af807fe04cd983be849853f9cc4a3a83bb8d5
unknown <abdunnoorgalib@gmail.com>
2025-02-06 00:34:14 -0400

    Contains Duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i:nums){
+            if(hs.contains(i)){
+                return true;
+            }else{
+                hs.add(i);
+            }
+        }
+        return false;
+    }
+}
--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
-        int nums[] = {2,7,11,15};
+        int nums[] = {1,2,3,1};
         System.out.println(Arrays.toString(ts.twoSum(nums, 9)));
         IsPalindrome pal = new IsPalindrome();
         System.out.println(pal.isPalindrome("A man, a plan, a canal: Panama"));
+        ContainsDuplicate cd = new ContainsDuplicate();
+        System.out.println(cd.containsDuplicate(nums));
     }
 }

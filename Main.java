664cb8d2f00bad876f6d49e8abe5af1da379a8d9
unknown <abdunnoorgalib@gmail.com>
2025-02-11 14:52:41 -0400

    268. Missing Number

--- Main.java
+++ Main.java
@@ -3,15 +3,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
-        int nums[] = {1,2,3,1};
-        System.out.println(Arrays.toString(ts.twoSum(nums, 9)));
-        IsPalindrome pal = new IsPalindrome();
-        System.out.println(pal.isPalindrome("A man, a plan, a canal: Panama"));
-        ContainsDuplicate cd = new ContainsDuplicate();
-        System.out.println(cd.containsDuplicate(nums));
-        IsAnagram ia = new IsAnagram();
-        System.out.println(ia.isAnagram("anagram", "nagaram"));
-        UniquePaths up = new UniquePaths();
-        System.out.println(up.uniquePaths(3,7));
+        int nums[] = {3,0,1};
+//        System.out.println(Arrays.toString(ts.twoSum(nums, 9)));
+//        IsPalindrome pal = new IsPalindrome();
+//        System.out.println(pal.isPalindrome("A man, a plan, a canal: Panama"));
+//        ContainsDuplicate cd = new ContainsDuplicate();
+//        System.out.println(cd.containsDuplicate(nums));
+//        IsAnagram ia = new IsAnagram();
+//        System.out.println(ia.isAnagram("anagram", "nagaram"));
+//        UniquePaths up = new UniquePaths();
+//        System.out.println(up.uniquePaths(3,7));
+        MissingNumber mn = new MissingNumber();
+        System.out.println(mn.missingNumber(nums));
     }
 }
